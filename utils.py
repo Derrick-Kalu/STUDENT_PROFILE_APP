186c1d204ab5931e0191c17bda0699bee2b5f1b1
joelpraise022 <159132107+joelpraise022@users.noreply.github.com>
2024-08-07 01:38:27 +0100

    Add files via upload
    This is my contribution to the app development

---
+++ utils.py
@@ -0,0 +1,11 @@
+def student_dict(student):
+    return {
+        "id": student.id,
+        "full_name" : student.full_name,
+        "matric_number": student.matric_number,
+        "gender": student.gender,
+        "level": student.level,
+        "department": student.department,
+        "hostel": student.hostel,
+        "room": student.room
+    }
